08bd327199ea0e74b627fe0191794c78f6516826
Your Name <you@example.com>
2020-12-10 20:08:09 +0800

    Improve server-side image validation

--- esignature/forms.py
+++ esignature/forms.py
@@ -20,21 +20,32 @@ class SignatureForm(forms.ModelForm):
     def clean(self):
         cleaned_data = super().clean()
         base64_sig = cleaned_data.get('signature')
-        if not base64_sig.startswith("data:image/png;base64,"):
+        if base64_sig==None:
             error_msg = "There was an issue uploading your signature. \
                 Please try again."
             self.add_error('signature', error_msg)
-        elif base64_sig == 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAASwAAACWCAYAAABkW7XSAAAAxUlEQVR4nO3BMQEAAADCoPVPbQhfoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAOA1v9QAATX68/0AAAAASUVORK5CYII=':
+        elif not base64_sig.startswith("data:image/png;base64,"):
+            error_msg = "There was an issue uploading your signature. \
+                Please try again."
+            self.add_error('signature', error_msg)
+        elif base64_sig == 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAASw\
+            AAACWCAYAAABkW7XSAAAAxUlEQVR4nO3BMQEAAADCoPVPbQhfoAAAAAAAAAAAAAAA\
+                AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\
+                    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\
+                        AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA\
+                            AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAOA1\
+                                v9QAATX68/0AAAAASUVORK5CYII=':
             error_msg = "Signature cannot be blank."
             self.add_error('signature', error_msg)
-        data_format, img_str = base64_sig.split(';base64,') 
-        ext = data_format.split('/')[-1]
-        self.instance.signature = ContentFile(
-            base64.b64decode(img_str),
-            name = '{date_time}-{unique_ref}.{file_extension}'.format(
-                date_time = now().strftime('%Y%m%d-%H:%M'),
-                unique_ref = str(uuid.uuid4()),
-                file_extension = ext,
+        else:
+            data_format, img_str = base64_sig.split(';base64,') 
+            ext = data_format.split('/')[-1]
+            self.instance.signature = ContentFile(
+                base64.b64decode(img_str),
+                name = '{date_time}-{unique_ref}.{file_extension}'.format(
+                    date_time = now().strftime('%Y%m%d-%H:%M'),
+                    unique_ref = str(uuid.uuid4()),
+                    file_extension = ext,
+                )
             )
-        )
         return cleaned_data
