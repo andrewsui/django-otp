d21188e0ba0ee026f9ef8f8b7f1dfd57d2f34985
andrewsui <67029820+andrewsui@users.noreply.github.com>
2020-12-13 21:28:01 +0800

    Use common input template for signature form

--- esignature/views.py
+++ esignature/views.py
@@ -11,10 +11,11 @@ from .forms import SignatureImageUrlForm, SignatureStringForm
 class SignatureImageUrlCreateView(CreateView):
     model = SignatureImageUrl
     form_class = SignatureImageUrlForm
+    template_name = 'esignature/signature_form.html'
     success_url = reverse_lazy('signature_image_create_route')
 
 class SignatureStringCreateView(CreateView):
     model = SignatureString
     form_class = SignatureStringForm
-    template_name = 'esignature/signatureimageurl_form.html'
+    template_name = 'esignature/signature_form.html'
     success_url = reverse_lazy('signature_string_create_route')
